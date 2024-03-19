9d6e1e0ef56abe46ae23dc02561b442543af1112
marcelobr <prueba@correo.com>
2024-03-18 21:33:14 -0500

    Tu mensaje de confirmación aquí

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require("tailwindcss/colors");
+const fontSize=require("tailwindcss/defaultTheme").fontSize;
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#0866F1",
+      "primary-dark": "#2377FF",
+      ...colors,
+    },
+    fontSize: {
+      ...fontSize,
+      regular: "15px",
+    }
+  },
+  plugins: [],
+  darkMode: "class",
+};
+
